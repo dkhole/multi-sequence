0ed0e11db041ef0bebc0f16708d8f8c9ccfbc1d1
dkhole <dkhlee419@gmail.com>
2021-03-30 09:43:17 +1100

    created rooms with nanoid, redoing logic on client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
   const [username, setUsername] = useState('');
+  const [roomId, setRoomId] = useState('');
+  const [inputRoom, setInputRoom] = useState('');
 
-  useEffect(() => {
-
-    const HOST = window.location.origin.replace(/^http/, 'ws');
-    const ws = new WebSocket(HOST);
+  const createRoom = () => {
+    //const HOST = window.location.origin.replace(/^http/, 'ws');
+    const ws = new WebSocket('ws://localhost:3005/');
 
     ws.onopen = () => {
-      console.log('websocket is connected');
-      ws.send("hello server");
+      const userInfo = {
+        type: 'create',
+        username: username,
+      }
+      ws.send(JSON.stringify(userInfo));
     };
 
-  }, []);
+    ws.onmessage = (msg: any) => {
+      const data = JSON.parse(msg.data);
+      console.log(msg);
+      if(data.type === 'roomId') {
+        setRoomId(data.value);
+      }
+    }
+  };
+
+  const joinRoom = () => {
+    const ws = new WebSocket('ws://localhost:3005/');
+
+    ws.onopen = () => {
+      const userInfo = {
+        type: 'join',
+        username: username,
+        roomId: inputRoom
+      }
+      ws.send(JSON.stringify(userInfo));
+    };
+
+    ws.onmessage = (msg: any) => {
+      const data = JSON.parse(msg.data);
+      console.log(msg);
+      if(data.type === 'found') {
+        console.log('found');
+      } else {
+        console.log('lost');
+      }
+    };
+  }
 
   return (
     <div className="App">
       <input type="text" value={username} onChange={(e) => {setUsername(e.target.value)}}></input>
-      <button>Create Room</button>
-      <button>Join Room</button>
+      <button onClick={createRoom}>Create Room</button>
+      <input type="text" value={inputRoom} onChange={(e) => {setInputRoom(e.target.value)}}></input>
+      <button onClick={joinRoom}>Join Room</button>
       <div>{username}</div>
+      <div>{roomId}</div>
     </div>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import WebSocket from 'ws';
+import { nanoid } from 'nanoid';
 
 const app = express();
 const PORT = 3005;
@@ -16,10 +17,69 @@ const server = app.listen(PORT, () => {
 
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
-    ws.on('message', (msg) => {
-        console.log(`recieved message: ${msg}`);
-    });
+let rooms: any = [];
 
-    ws.send('sent msg');
+wss.on('connection', (ws) => {
+    ws.on('message', (msg: string) => {
+        
+        const data = JSON.parse(msg);
+
+        if(data.type === 'create') {
+            //create uuid and room object
+            const room = {
+                id: nanoid(10),
+                user1: ws,
+                user1_id: data.username,
+                user2: null,
+                user2_id: ''
+            }
+
+            rooms.push(room);
+
+            const res = {
+                type: 'roomId',
+                value: room.id.toString()
+            }
+
+            ws.send(JSON.stringify(res));
+            console.log(rooms);
+        }
+
+        if(data.type === 'join') {
+            let found = false;
+            let findRoom;
+            //search for roomid and add user if found
+            for(const room of rooms) {
+                if(room.id === data.roomId) {
+                    room.user2 = ws;
+                    room.user2_id = data.username;
+                    found = true;
+                    findRoom = room;
+                }
+            }
+
+            if(found) {
+                const res = {
+                    type: 'found',
+                    user1_id: findRoom.user1_id,
+                    user2_id: findRoom.user2_id,
+                }
+                ws.send(JSON.stringify(res));
+
+                for(const room of rooms) {
+                    if(room.user1 && room.user2) {
+                        room.user1.send(JSON.stringify({status: 'connected'}));
+                        room.user2.send(JSON.stringify({status: 'connected'}));
+                        console.log('broadcasted to room');
+                    }
+                }
+            } else {
+                const res = {
+                    type: 'invalid id',
+                }
+                ws.send(JSON.stringify(res));
+            }
+        }
+    });
 })
+
